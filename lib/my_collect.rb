a9728395bbcf8acb2a806fcb5569bfbc79055f12
Tristan Brown <tgbrown@stanford.edu>
2018-08-22 20:15:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+ i = 0
+ array = []
+ while i < array.length
+   array << yield(collection[i])
+   i += 1
+ end 
+ array 
+ end 
+
+ collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+
 
 
