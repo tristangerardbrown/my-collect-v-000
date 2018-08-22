94e8da4be7947c8d310d412278a98f7ae506d625
Tristan Brown <tgbrown@stanford.edu>
2018-08-22 20:20:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+ i = 0
+ array = []
+ while i < collection.length
+   array << yield(collection[i])
+   i += 1
+ end
+ array
+ end
 
-
+ collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
